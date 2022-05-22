e312d029b4c5be4ddcf0c9ffe61f62527e7f6264
Dawood Khan Masood <91176669+DawoodKMasood@users.noreply.github.com>
2022-05-22 15:37:48 +0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://api.meraqanoon.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
